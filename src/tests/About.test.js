756344ebb276a0b4b6c3f859577ce0de4c7bde47
Helena <nenafgreco@hotmail.com>
2021-09-29 20:32:27 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,21 +1,35 @@
-// import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { screen } from '@testing-library/react';
 // import userEvent from '@testing-library/user-event';
-// import App from '../App';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
 
-// describe('2. Teste o componente <About.js />.', () => {
-//   test('se a página contém as informações sobre a Pokédex.', () => {
+describe('2. Teste o componente <About.js />.', () => {
+  test('se a página contém um heading h2 com o texto About Pokédex.', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/about');
+    const title = screen.getByRole('heading', {
+      level: 2,
+      name: 'About Pokédex',
+    });
 
-//   });
+    expect(title).toBeInTheDocument();
+  });
 
-//   test('se a página contém um heading h2 com o texto About Pokédex.', () => {
+  /** Ref: https://testing-library.com/docs/queries/about/#textmatch sobre substrings no TextMatch */
+  test('se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/about');
+    const p1 = screen.getByText(/This application simulates a Pokédex/);
+    const p2 = screen.getByText(/One can filter Pokémons by type/);
+    expect(p1).toBeInTheDocument();
+    expect(p2).toBeInTheDocument();
+  });
 
-//   });
-
-//   test('se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
-
-//   });
-
-//   test('se a página contém a seguinte imagem de uma Pokédex: https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png.', () => {
-
-//   });
-// });
+  test('se a página contém a seguinte imagem de uma Pokédex: https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png.', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/about');
+    const pokedexImg = screen.getByRole('img');
+    expect(pokedexImg).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
