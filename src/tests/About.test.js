4cae76ba44c1117e47b7b37907ece2a859f8fc9a
Helena <nenafgreco@hotmail.com>
2021-09-29 19:40:48 -0300

    corrigindo erros

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,21 @@
-test('a', () => {});
+// import { render, screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// import App from '../App';
+
+// describe('2. Teste o componente <About.js />.', () => {
+//   test('se a página contém as informações sobre a Pokédex.', () => {
+
+//   });
+
+//   test('se a página contém um heading h2 com o texto About Pokédex.', () => {
+
+//   });
+
+//   test('se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+
+//   });
+
+//   test('se a página contém a seguinte imagem de uma Pokédex: https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png.', () => {
+
+//   });
+// });
