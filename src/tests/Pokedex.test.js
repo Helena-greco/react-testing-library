fd5d4fd37d98c57fc35f4522042f38722896a7aa
Helena <nenafgreco@hotmail.com>
2021-10-07 18:43:41 -0300

    Requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
@@ -20,5 +21,38 @@ describe('5. Teste o componente <Pokedex.js />', () => {
       name: 'Próximo pokémon',
     });
     expect(buttonName).toBeInTheDocument();
+  });
+
+  test('se só mostra um pokemon por vez', () => {
+    renderWithRouter(<App />);
+    const title = screen.getAllByTestId('pokemon-name');
+    expect(title.length).toBe(1);
+  });
+
+  test('se a Pokédex tem os botões de filtros', () => {
+    renderWithRouter(<App />);
+    const buttonLength = 7;
+    const filterButtons = screen.getAllByTestId('pokemon-type-button');
+    expect(filterButtons.length).toBe(buttonLength);
+
+    // verificar se cada botão tem o nome de cada filtro.
+    filterButtons.forEach((button, index) => {
+      userEvent.click(button);
+      const typeButton = screen.getByTestId('pokemon-type');
+      expect(filterButtons[index].innerHTML).toBe(typeButton.innerHTML);
+    });
+
+    const allBtn = screen.getByRole('button', { name: 'All' });
+    expect(allBtn).toBeInTheDocument();
+  });
+
+  test('se a Pokédex contém um botão para resetar o filtro', () => {
+    renderWithRouter(<App />);
+    const allBtn = screen.getByRole('button', { name: 'All' });
+    expect(allBtn).toBeInTheDocument();
+
+    userEvent.click(allBtn);
+    const verifyPokemonName = screen.getByTestId('pokemon-name');
+    expect(verifyPokemonName).toBeInTheDocument();
   });
 });
